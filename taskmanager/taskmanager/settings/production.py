2298907c51f9221405d773684118a55ade3bc790
George Zhukov <george@freebsd-vps.lan>
2015-03-24 01:54:55 +0200

    added prod config

--- taskmanager/taskmanager/settings/production.py
+++ taskmanager/taskmanager/settings/production.py
@@ -1,2 +1,11 @@
 from .base import *
 
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'taskmanager',
+        'USER': 'django',
+    }
+}
+
