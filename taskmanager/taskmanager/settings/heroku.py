c08442c72452264685c52d1532781e178547f072
George Zhukov <scofield.cross@gmail.com>
2015-03-24 00:48:21 +0200

    try different static root

--- taskmanager/taskmanager/settings/heroku.py
+++ taskmanager/taskmanager/settings/heroku.py
@@ -11,9 +11,5 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
