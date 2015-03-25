c3baf9ebb756963f06be614c7cbcac257319a14b
George Zhukov <scofield.cross@gmail.com>
2015-03-25 16:35:08 +0200

    clear urls

--- taskmanager/tasks/urls.py
+++ taskmanager/tasks/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, include, url
 from .views import AngularView
 
 urlpatterns = patterns('',
-     url(r'^angular/$', AngularView.as_view()),
+     url(r'^$', AngularView.as_view()),
 )
