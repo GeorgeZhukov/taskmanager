91063c7499224a5326a81e8f4fc4a9708aa0eda3
George Zhukov <scofield.cross@gmail.com>
2015-03-26 19:30:31 +0200

    new front-end tests structure with karma

---
+++ test/test.js
@@ -0,0 +1,20 @@
+describe('Task', function () {
+//
+    //var Task;
+    //var Project;
+    //beforeEach(module('services'));
+    //beforeEach(inject(function (_Project_, _Task_) {
+    //    Project = _Project_;
+    //    Task = _Task_;
+    //}));
+
+    describe('Constructor', function () {
+
+        it('assigns a content', function () {
+            expect(1).toEqual(1);
+            //expect(new Task('Task 1')).to.have.property('content', 'Task 1');
+        });
+
+    });
+
+});
